9e2a24f665641fbe85c4cfa9260ed7e5265994d5
nikolaychirkov <nikolay4irkov@gmail.com>
2021-03-17 21:17:56 +0200

    attempt to animate planes on about page

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,13 +1,30 @@
 gsap.registerPlugin(ScrollTrigger);
+const $offset = 0;
+
+const $pageHeight = $(document).height();
+
+console.log($pageHeight);
+const $pageHeightIncreased = $pageHeight + $offset;
 
 gsap.to("#plane-up", {
   scrollTrigger: {
     trigger: "#hero",
     start: "top top",
-    end: "+=2000",
+    end: "+=3500",
     ease: "circ.out",
     scrub: 1,
   },
-  y: -4000,
-  duration: 0.7,
+  y: -$pageHeight,
+});
+
+gsap.to("#plane-down", {
+  scrollTrigger: {
+    trigger: "#hero",
+    start: "top top",
+    end: "+=3500",
+    ease: "circ.out",
+    scrub: 1,
+    // markers: true,
+  },
+  y: $pageHeightIncreased,
 });
