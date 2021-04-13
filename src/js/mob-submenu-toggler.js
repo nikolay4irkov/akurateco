b5ed52ff194b5953d0cc5535a69c26d10e11ae28
nikolaychirkov <nikolay4irkov@gmail.com>
2021-04-13 22:15:36 +0300

    header rebuild for SEO

--- src/js/mob-submenu-toggler.js
+++ src/js/mob-submenu-toggler.js
@@ -3,5 +3,6 @@ const $submenu = $(".js-submenu");
 
 $submenuToggler.on("click", function () {
   $(this).toggleClass("is-active");
-  $(this).closest(".submenu-wrapper").children(".js-submenu").slideToggle(300);
+  // $(this).closest(".submenu-wrapper").children(".js-submenu").slideToggle(300);
+  $(this).closest(".submenu-wrapper").find(".js-submenu").slideToggle(300);
 });
